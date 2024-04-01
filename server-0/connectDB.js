8fc430dcd0e56e78e6b37574c81ccf8cf24d4ea6
Surya <35captainmarvel@gmail.com>
2024-04-01 12:01:56 +0530

    updated connectDB and package.json

--- server-0/connectDB.js
+++ server-0/connectDB.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
 
+const link =
+  "mongodb+srv://lawlovesnicorobin:law4robin@surya.uxsgorx.mongodb.net/Books";
+
 const connectDB = async () => {
   try {
-    mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    mongoose.set("strictQuery", false);
+    const conn = await mongoose.connect(process.env.MONGODB_URL || link);
     console.log(`Database connected ${conn.connection.host}`);
-
   } catch (error) {
     console.log(error);
     process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
